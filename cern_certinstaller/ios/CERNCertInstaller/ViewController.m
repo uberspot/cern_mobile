0cfc129e687fc00948c8974e2c9eaaa817be4017
Paul Sarbinowski <uberspot@users.noreply.github.com>
2015-07-15 14:16:46 +0300

    Update ViewController.m

--- cern_certinstaller/ios/CERNCertInstaller/ViewController.m
+++ cern_certinstaller/ios/CERNCertInstaller/ViewController.m
@@ -2,9 +2,6 @@
 //  ViewController.m
 //  CERNCertInstaller
 //
-//  Created by Pawel Sarbinowski local on 7/4/13.
-//  Copyright (c) 2013 Pawel Sarbinowski local. All rights reserved.
-//
 
 #import "ViewController.h"
 
