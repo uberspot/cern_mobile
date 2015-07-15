0c60b70601a764a19d27973fc14dd5934e9671b6
Paul Sarbinowski <uberspot@users.noreply.github.com>
2015-07-15 14:16:43 +0300

    Update AppDelegate.h

--- cern_certinstaller/ios/CERNCertInstaller/AppDelegate.h
+++ cern_certinstaller/ios/CERNCertInstaller/AppDelegate.h
@@ -2,9 +2,6 @@
 //  AppDelegate.h
 //  CERNCertInstaller
 //
-//  Created by Pawel Sarbinowski local on 7/4/13.
-//  Copyright (c) 2013 Pawel Sarbinowski local. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 
